675ea1edc44edfdcad754d2e10ee91c22ec36677
Roberto Zegers <rzegers@puc.cl>
2018-09-10 05:52:41 -0700

    Add: complete code in __main__ Change: Subscribe to the camera data (point cloud) from topic /pr2/world/points

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -256,15 +256,33 @@ def pr2_mover(object_list):
 
 if __name__ == '__main__':
 
-    # TODO: ROS node initialization
+   # ROS node initialization (was TODO)
+    rospy.init_node('object_recognition', anonymous=True)
 
-    # TODO: Create Subscribers
+    # Create Subscribers (was TODO)
+    pcl_sub = rospy.Subscriber("/pr2/world/points", pc2.PointCloud2, pcl_callback, queue_size=1)
 
-    # TODO: Create Publishers
+    # Create Publishers 
+    pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
+    pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
+    pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
+    # Here you need to create two new publishers (was TODO)
+    # Call them object_markers_pub and detected_objects_pub
+    # Have them publish to "/object_markers" and "/detected_objects" with 
+    # Message Types "Marker" and "DetectedObjectsArray" , respectively
+    object_markers_pub = rospy.Publisher("/object_markers", Marker, queue_size=1)
+    detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size=1)
 
-    # TODO: Load Model From disk
+    # Load Model From disk (was TODO)
+    model = pickle.load(open('model.sav', 'rb'))
+    clf = model['classifier']
+    encoder = LabelEncoder()
+    encoder.classes_ = model['classes']
+    scaler = model['scaler']
 
     # Initialize color_list
     get_color_list.color_list = []
 
-    # TODO: Spin while node is not shutdown
+    # Spin while node is not shutdown (was TODO)
+    while not rospy.is_shutdown():
+        rospy.spin()
